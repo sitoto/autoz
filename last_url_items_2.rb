896fc5c32d3142f68408e6857f0c6f1ac53468f6
jiapei <huangw@parts.com>
2013-03-13 17:17:14 +0800

    add items

---
+++ last_url_items_2.rb
@@ -0,0 +1,5 @@
+@last_urls = %w(
+http://www.autozone.com/autozone/parts/Duralast-Brake-Rotor-Rear/_/N-8knrr?itemIdentifier=107292_0_0_
+http://www.autozone.com/autozone/parts/Duralast-Brake-Rotor-Rear/_/N-8knrr?itemIdentifier=112425_0_0_
+http://www.autozone.com/autozone/parts/Duralast-Brake-Rotor-Rear/_/N-8knrr?itemIdentifier=112433_0_0_
+)
