d74fa387e5c1e04513dbf94c4d735afa40513bd0
jiapei <huangw@parts.com>
2013-03-15 17:53:54 +0800

    add 2

--- last_url_items_2.rb
+++ last_url_items_2.rb
@@ -1,5 +1,10 @@
 @last_urls = %w(
-http://www.autozone.com/autozone/parts/Duralast-Brake-Rotor-Rear/_/N-8knrr?itemIdentifier=107292_0_0_
-http://www.autozone.com/autozone/parts/Duralast-Brake-Rotor-Rear/_/N-8knrr?itemIdentifier=112425_0_0_
-http://www.autozone.com/autozone/parts/Duralast-Brake-Rotor-Rear/_/N-8knrr?itemIdentifier=112433_0_0_
+
+http://www.autozone.com/autozone/parts/Duralast-Brake-Rotor-Front/_/N-8knrq?itemIdentifier=112524_0_0_
+
+http://www.autozone.com/autozone/parts/Duralast-Brake-Drum-Front/_/N-8knr7?itemIdentifier=113322_0_0_
+
+http://www.autozone.com/autozone/parts/Duralast-Brake-Drum-Front/_/N-8knr7?itemIdentifier=113326_0_0_
+
+
 )
